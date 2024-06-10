4a1099fec9b33dbbb8bb362fe04a380c6d01257f
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-06-10 13:54:51 +0300

    update

--- DBL/Entities/CustomerAccountTransfer.cs
+++ DBL/Entities/CustomerAccountTransfer.cs
@@ -5,6 +5,7 @@
         public long FromAccountId { get; set; }
         public long ToAccountId { get; set; }
         public long TransferId { get; set; }
+        public long TenantId { get; set; }
         public decimal TransferAmount { get; set; }
         public string? TransactionDescription { get; set; }
         public long CreatedbyId { get; set; }
